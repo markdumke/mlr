c4ca4921d6d805217cb0ae21ee6d70847165b081
Markus Dumke <markusdumke@gmail.com>
2017-11-16 15:04:31 +0100

    add reinfLearnTask for reinforcement learning

---
+++ R/ReinfLearnTask.R
@@ -0,0 +1,37 @@
+#' @rdname Task
+#' @export
+makeReinfLearnTask = function(id, envir) {
+  assertString(id)
+  assertClass(env, "R6")
+  task = makeS3Obj("Task",
+    type = "reinfLearn",
+    task.desc = NA
+  )
+  task$task.desc = makeReinfLearnTaskDesc(id, envir)
+  addClasses(task, "ReinfLearnTask")
+}
+
+makeReinfLearnTaskDesc = function(id, envir) {
+  td = makeReinfLearnTaskDescInternal("reinfLearn", id, envir)
+  return(addClasses(td, c("reinfLearnTaskDesc")))
+}
+
+makeReinfLearnTaskDescInternal = function(type, id, envir) {
+
+  makeS3Obj("TaskDesc",
+    id = id,
+    type = type,
+    states = envir$n.states,
+    actions = envir$n.actions
+  )
+}
+
+
+#' @export
+print.ReinfLearnTask = function(x, ...) {
+  td = x$task.desc
+  catf("Reinforcement learning task: %s", td$id)
+  catf("Type: %s", td$type)
+  catf("States: %i", td$states)
+  catf("Actions: %i", td$actions)
+}
